574dd3659511141450096282e8eaedfee8cf44b4
mateuso12 <mateuso12@hotmail.com.br>
2023-02-22 15:47:31 -0300

    build: config issue route

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const userApi = axios.create({
   baseURL: 'https://api.github.com/users/mateuso12',
 })
+
+export const issueApi = axios.create({
+  baseURL: 'https://api.github.com/repos/mateuso12/github-blog-posts/issues',
+})
